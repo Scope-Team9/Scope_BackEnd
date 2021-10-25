2c8b0a50641ce7a5c34e2ac11b0a01cbd743c5ef
Kwon Min Hyeok <rnjsals1575@naver.com>
2021-10-25 18:25:14 +0900

    add

---
+++ src/main/java/com/studycollaboproject/scope/ScopeApplication.java
@@ -0,0 +1,13 @@
+package com.studycollaboproject.scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScopeApplication.class, args);
+    }
+
+}
