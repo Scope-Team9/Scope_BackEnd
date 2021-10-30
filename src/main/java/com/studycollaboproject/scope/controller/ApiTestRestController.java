6d5afb7f35c33eacdfcce63e3e28dc0c9c865bc0
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-31 03:43:00 +0900

    [수정] api test

--- src/main/java/com/studycollaboproject/scope/controller/ApiTestRestController.java
+++ src/main/java/com/studycollaboproject/scope/controller/ApiTestRestController.java
@@ -7,22 +7,26 @@ import com.studycollaboproject.scope.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RequiredArgsConstructor
 @RestController
 public class ApiTestRestController {
 
-    UserRepository userRepository;
-    JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/api-test")
-    public String  makeApiTestUser(@ModelAttribute("snsId") String snsId,
+    public Map<String,String>  makeApiTestUser(@ModelAttribute("snsId") String snsId,
                                    @ModelAttribute("userPropensityType") String userPropensityType,@ModelAttribute("memberPropensityType") String memberPropensityType,
                                    @ModelAttribute("nickname") String nickname, @ModelAttribute("email") String email){
         User user = new User(snsId,userPropensityType,memberPropensityType,nickname,email);
         userRepository.save(user);
-
-        return jwtTokenProvider.createToken(snsId);
+        String jwtToken =jwtTokenProvider.createToken(snsId);
+        Map<String ,String> token = new HashMap<>();
+        token.put("token",jwtToken);
+        return token;
     }
 }
