54f41eb62272e362dccdfbb5337906f0f9a2840d
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-01 02:56:03 +0900

    [수정] 마이페이지 조회 수정

--- src/main/java/com/studycollaboproject/scope/dto/MypagePostListDto.java
+++ src/main/java/com/studycollaboproject/scope/dto/MypagePostListDto.java
@@ -1,13 +1,14 @@
 package com.studycollaboproject.scope.dto;
 
 import com.studycollaboproject.scope.model.Post;
+import com.studycollaboproject.scope.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-public class PostListDto {
+public class MypagePostListDto {
 
     @Schema(description = "북마크 프로젝트 리스트")
     private List<Post> boomark;
@@ -17,11 +18,17 @@ public class PostListDto {
     private List<Post> inProgress;
     @Schema(description = "완료된 프로젝트 리스트")
     private List<Post> end;
+    @Schema(description = "사용자 본인의 마이페이지인지 판단")
+    private boolean isMyMypage;
+    @Schema(description = "조회하고자 하는 유저")
+    private UserResponseDto userResponseDto;
 
-    public PostListDto(List<Post> bookmark, List<Post> recruitment, List<Post> inProgress, List<Post> end) {
+    public MypagePostListDto(UserResponseDto user,  List<Post> recruitment, List<Post> inProgress,List<Post> end) {
         this.recruitment = recruitment;
         this.end = end;
         this.inProgress = inProgress;
-        this.boomark = bookmark;
+        this.userResponseDto = user;
+
+
     }
 }
