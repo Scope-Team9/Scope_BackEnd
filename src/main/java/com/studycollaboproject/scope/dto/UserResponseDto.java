6be6fd578f060ccebe1bb8b9aa68887ff21b3f99
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-01 04:06:00 +0900

    [추가] 스웨거 추가

--- src/main/java/com/studycollaboproject/scope/dto/UserResponseDto.java
+++ src/main/java/com/studycollaboproject/scope/dto/UserResponseDto.java
@@ -2,21 +2,22 @@ package com.studycollaboproject.scope.dto;
 
 import com.studycollaboproject.scope.model.TechStack;
 import com.studycollaboproject.scope.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class UserResponseDto {
-
+    @Schema(description = "사용자 성향")
     private String userPropensityType;
-
+    @Schema(description = "사용자의 선호 팀원 성향")
     private String memberPropensityType;
-
+    @Schema(description = "닉네임")
     private String nickname;
-
+    @Schema(description = "소개")
     private String introduction;
-
+    @Schema(description = "기술 스택 리스트")
     private List<TechStack> techStackList;
 
 
