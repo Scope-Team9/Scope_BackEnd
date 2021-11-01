eb964e2262c9614ea435e914ed633cf5bb68d3f8
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-01 21:15:43 +0900

    [추가] api test용 로그인 api 추가

--- src/main/java/com/studycollaboproject/scope/controller/ApiTestRestController.java
+++ src/main/java/com/studycollaboproject/scope/controller/ApiTestRestController.java
@@ -4,6 +4,8 @@ import com.studycollaboproject.scope.dto.SignupTestDto;
 import com.studycollaboproject.scope.model.User;
 import com.studycollaboproject.scope.repository.UserRepository;
 import com.studycollaboproject.scope.security.JwtTokenProvider;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +25,14 @@ public class ApiTestRestController {
         User user = new User(signupTestDto);
         userRepository.save(user);
         String jwtToken =jwtTokenProvider.createToken(signupTestDto.getSnsId());
+        Map<String ,String> token = new HashMap<>();
+        token.put("token",jwtToken);
+        return token;
+    }
+
+    @GetMapping("/api-test")
+    public Map<String ,String > loginApiTestUser(@Parameter(description = "닉네임", in = ParameterIn.QUERY)@RequestParam String snsId){
+        String jwtToken =jwtTokenProvider.createToken(snsId);
         Map<String ,String> token = new HashMap<>();
         token.put("token",jwtToken);
         return token;
