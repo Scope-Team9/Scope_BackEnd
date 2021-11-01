cfbb2b6195504b414bad7015d8ecd56f9dd0442f
권민혁 <rnjsals1575@naver.com>
2021-11-02 02:05:13 +0900

    Update ServerRestController.java

--- src/main/java/com/studycollaboproject/scope/controller/ServerRestController.java
+++ src/main/java/com/studycollaboproject/scope/controller/ServerRestController.java
@@ -17,7 +17,7 @@ public class ServerRestController {
     public String getProfile() {
         // jar파일 실행 시 설정한 실행 값을 배열로 저장(0번째 리스트에 저장됨)
         List<String> profile = Arrays.asList(env.getActiveProfiles());
-        // rea1과 rea2를 리스트로 저장
+        // real1과 real2를 리스트로 저장
         List<String> realProfiles = Arrays.asList("real1", "real2");
         // 만약 default가 아니면 현재 실행중 값 반환
         String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
