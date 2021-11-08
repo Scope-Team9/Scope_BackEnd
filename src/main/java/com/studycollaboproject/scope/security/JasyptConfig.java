3d4b77be1a000020f0605064588c8c74a1755b2a
Kwon Min Hyeok <rnjsals1575@naver.com>
2021-11-09 01:11:47 +0900

    [수정] properties 수정

--- src/main/java/com/studycollaboproject/scope/security/JasyptConfig.java
+++ src/main/java/com/studycollaboproject/scope/security/JasyptConfig.java
@@ -4,21 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @RequiredArgsConstructor
 @Configuration
 public class JasyptConfig {
-    @Value("${jasypt.encryptor.password}")
-    private String password;
+    private final Environment env;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
+        config.setPassword(env.getProperty("jasypt.encryptor.password"));
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
