dc8c00dd09316f4e9bb28288f1419cf360bf5ea8
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-18 00:52:17 +0900

    [추가] 생성자 추가

--- src/main/java/com/studycollaboproject/scope/dto/ApplicantRequestDto.java
+++ src/main/java/com/studycollaboproject/scope/dto/ApplicantRequestDto.java
@@ -1,11 +1,13 @@
 package com.studycollaboproject.scope.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "지원 요청")
 public class ApplicantRequestDto {
     @Schema(description = "한줄 소개")
