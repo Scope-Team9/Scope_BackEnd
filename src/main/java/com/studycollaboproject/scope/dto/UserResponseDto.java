7a99e25bcac351efe97cad3fe8c8d087bcff8df2
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-26 02:16:15 +0900

    [수정] cpp -> c++

--- src/main/java/com/studycollaboproject/scope/dto/UserResponseDto.java
+++ src/main/java/com/studycollaboproject/scope/dto/UserResponseDto.java
@@ -32,13 +32,13 @@ public class UserResponseDto {
         this.introduction = user.getIntroduction();
         this.memberPropensityType = user.getMemberPropensityType();
         this.nickname = user.getNickname();
-        this.techStackList = techStackList;
         for (int i=0;i<techStackList.size();i++){
             if (techStackList.get(i).equals("cpp")){
                 techStackList.set(i,"C++");
                 break;
             }
         }
+        this.techStackList = techStackList;
         this.userId = user.getId();
         this.email = user.getEmail();
         this.isVerifiedEmail = user.getIsVerifiedEmail();
