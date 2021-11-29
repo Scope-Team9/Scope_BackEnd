cb441fe0392b02f28adb6b594793acfd1ade176a
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-30 06:02:31 +0900

    [추가] 동시 접속자 수 확인 위한 로직 수정

--- src/main/java/com/studycollaboproject/scope/controller/HomeController.java
+++ src/main/java/com/studycollaboproject/scope/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@RequiredArgsConstructor
+
 @Controller
 public class HomeController {
 
