3223c41636dd8f0973dc073f7e73d7f485a82b99
9sanha <64877575+9sanha@users.noreply.github.com>
2021-11-30 04:49:07 +0900

    [추가] 동시 접속자 수 확인 위한 로직 추가

---
+++ src/main/java/com/studycollaboproject/scope/listener/SessionUserCounter.java
@@ -0,0 +1,34 @@
+package com.studycollaboproject.scope.listener;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@Slf4j
+@Getter
+@WebListener
+public class SessionUserCounter implements HttpSessionListener {
+    public static int count;
+
+    //세션이 만들어질 때 실행
+    @Override
+    public void sessionCreated(HttpSessionEvent event) {
+        HttpSession session =event.getSession();
+        count++;
+        log.info("세션 생성 : {}, 동시 접속자 : {}명",session.getId(),count);
+    }
+
+    //세션이 소멸될 때 실행행
+   @Override
+    public void sessionDestroyed(HttpSessionEvent event) {
+        if (count!=0){
+            count--;
+        }
+       HttpSession session =event.getSession();
+       log.info("세션 소멸 : {} 동시 접속자 : {}명",session.getId(),count);
+    }
+}
