45267a267cdc652bb95419f0540e682576ad954a
Kwon Min Hyeok <rnjsals1575@naver.com>
2021-12-02 07:00:10 +0900

    [추가] EhCache 추가 (post부분)

--- src/main/java/com/studycollaboproject/scope/ScopeApplication.java
+++ src/main/java/com/studycollaboproject/scope/ScopeApplication.java
@@ -2,12 +2,14 @@ package com.studycollaboproject.scope;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.util.TimeZone;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class ScopeApplication {
