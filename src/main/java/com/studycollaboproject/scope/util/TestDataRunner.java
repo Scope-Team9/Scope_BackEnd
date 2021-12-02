4a5c75442c61e323442b8ad628bff8c0f4c2d3b4
woodimora <woodimora@gmail.com>
2021-12-02 19:52:27 +0900

    [수정] mail validation, 조회 쿼리 수정

--- src/main/java/com/studycollaboproject/scope/util/TestDataRunner.java
+++ src/main/java/com/studycollaboproject/scope/util/TestDataRunner.java
@@ -1,64 +1,65 @@
-package com.studycollaboproject.scope.controller;
-
-import com.studycollaboproject.scope.dto.PostRequestDto;
-import com.studycollaboproject.scope.dto.SignupTestDto;
-import com.studycollaboproject.scope.dto.TestUserSetupDto;
-import com.studycollaboproject.scope.model.TechStack;
-import com.studycollaboproject.scope.model.TotalResult;
-import com.studycollaboproject.scope.model.User;
-import com.studycollaboproject.scope.repository.TechStackRepository;
-import com.studycollaboproject.scope.repository.TotalResultRepository;
-import com.studycollaboproject.scope.repository.UserRepository;
-import com.studycollaboproject.scope.security.JwtTokenProvider;
-import com.studycollaboproject.scope.service.PostService;
-import com.studycollaboproject.scope.util.TechStackConverter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@RequiredArgsConstructor
-@Component
-public class TestDataRunner implements ApplicationRunner {
-
-    private final TotalResultRepository totalResultRepository;
-    private final UserRepository userRepository;
-    private final PostService postService;
-    private final TechStackConverter techStackConverter;
-    private final TechStackRepository techStackRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Override
-    public void run(ApplicationArguments args) {
-        if (totalResultRepository.count() == 0L) {
-            String[] propensityTypeList = {"LVG", "LVP", "LHG", "LHP", "FVG", "FVP", "FHG", "FHP", "RHP"};
-
-            for (String s : propensityTypeList) {
-                for (String s1 : propensityTypeList) {
-                    TotalResult totalResult = new TotalResult(s, s1);
-                    totalResultRepository.save(totalResult);
-                }
-            }
-        }
-
-        if (userRepository.findBySnsId("unknown").isEmpty()) {
-            User user = new User(new TestUserSetupDto());
-            userRepository.save(user);
-        }
-//        for (PostRequestDto requestDto : requestDtoList) {
-//            System.out.println("requestDto = " + requestDto);
-//        }
-//        if(userRepository.count() > 50){
-//            return;
+//package com.studycollaboproject.scope.util;
+//
+//import com.studycollaboproject.scope.dto.PostRequestDto;
+//import com.studycollaboproject.scope.dto.SignupTestDto;
+//import com.studycollaboproject.scope.dto.TestUserSetupDto;
+//import com.studycollaboproject.scope.model.TechStack;
+//import com.studycollaboproject.scope.model.TotalResult;
+//import com.studycollaboproject.scope.model.User;
+//import com.studycollaboproject.scope.repository.TechStackRepository;
+//import com.studycollaboproject.scope.repository.TotalResultRepository;
+//import com.studycollaboproject.scope.repository.UserRepository;
+//import com.studycollaboproject.scope.security.JwtTokenProvider;
+//import com.studycollaboproject.scope.service.PostService;
+//import com.studycollaboproject.scope.util.TechStackConverter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.sql.Timestamp;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//@RequiredArgsConstructor
+//@Component
+//public class TestDataRunner implements ApplicationRunner {
+//
+//    private final TotalResultRepository totalResultRepository;
+//    private final UserRepository userRepository;
+//    private final PostService postService;
+//    private final TechStackConverter techStackConverter;
+//    private final TechStackRepository techStackRepository;
+//    private final JwtTokenProvider jwtTokenProvider;
+//
+//    @Override
+//    public void run(ApplicationArguments args) {
+//        if (totalResultRepository.count() == 0L) {
+//            String[] propensityTypeList = {"LVG", "LVP", "LHG", "LHP", "FVG", "FVP", "FHG", "FHP", "RHP"};
+//
+//            for (String s : propensityTypeList) {
+//                for (String s1 : propensityTypeList) {
+//                    TotalResult totalResult = new TotalResult(s, s1);
+//                    totalResultRepository.save(totalResult);
+//                }
+//            }
 //        }
 //
+//        if (userRepository.findBySnsId("unknown").isEmpty()) {
+//            User user = new User(new TestUserSetupDto());
+//            userRepository.save(user);
+//        }
+////        for (PostRequestDto requestDto : requestDtoList) {
+////            System.out.println("requestDto = " + requestDto);
+////        }
+////        if(userRepository.count() > 50){
+////            return;
+////        }
+////
 //        List<String> propensityType = new ArrayList<>();
 //
 //        propensityType.add("LHP");
@@ -73,33 +74,33 @@ public class TestDataRunner implements ApplicationRunner {
 //        propensityType.add("RHP");
 //
 //
-//        List<String> techStack = new ArrayList<>();
-//        techStack.add("Java");
-//        techStack.add("JavaScript");
-//        techStack.add("Python");
-//        techStack.add("Node");
-//        techStack.add("cpp");
-//        techStack.add("Flask");
-//        techStack.add("Django");
-//        techStack.add("React");
-//        techStack.add("php");
-//        techStack.add("Swift");
-//        techStack.add("Kotlin");
-//        techStack.add("TypeScript");
-//        techStack.add("Spring");
-
+//        List<String> TechStack = new ArrayList<>();
+//        TechStack.add("Java");
+//        TechStack.add("JavaScript");
+//        TechStack.add("Python");
+//        TechStack.add("Node");
+//        TechStack.add("cpp");
+//        TechStack.add("Flask");
+//        TechStack.add("Django");
+//        TechStack.add("React");
+//        TechStack.add("php");
+//        TechStack.add("Swift");
+//        TechStack.add("Kotlin");
+//        TechStack.add("TypeScript");
+//        TechStack.add("Spring");
+//
 //        List<List<String>> techStackList = new ArrayList<>();
-
-//        for (int i = 0; i < techStack.size(); i++) {
-//            for (int j = i + 1; j < techStack.size(); j++) {
+//
+//        for (int i = 0; i < TechStack.size(); i++) {
+//            for (int j = i + 1; j < TechStack.size(); j++) {
 //                List<String> techStackStringList = new ArrayList<>();
-//                techStackStringList.add(techStack.get(i));
-//                techStackStringList.add(techStack.get(j));
+//                techStackStringList.add(TechStack.get(i));
+//                techStackStringList.add(TechStack.get(j));
 //
 //                techStackList.add(techStackStringList);
 //            }
 //        }
-
+//
 //        List<User> userList = new ArrayList<>();
 //
 //        int index1 = 0;
@@ -128,8 +129,7 @@ public class TestDataRunner implements ApplicationRunner {
 //                techStacks.add(techStack1);
 //                techStackRepository.save(techStack1);
 //            }
-//            String token1 = jwtTokenProvider.createToken(user.getSnsId());
-//            user.addTechStackListAndToken(techStacks,token1);
+//            user.addTechStackList(techStacks);
 //            userRepository.save(user);
 //            userList.add(user);
 //        }
@@ -225,20 +225,20 @@ public class TestDataRunner implements ApplicationRunner {
         /*
         랜덤 데이터 100개 생성
          */
-//        List<String> techStack = new ArrayList<>();
-//        techStack.add("Java");
-//        techStack.add("JavaScript");
-//        techStack.add("Python");
-//        techStack.add("Node");
-//        techStack.add("cpp");
-//        techStack.add("Flask");
-//        techStack.add("Django");
-//        techStack.add("React");
-//        techStack.add("php");
-//        techStack.add("Swift");
-//        techStack.add("Kotlin");
-//        techStack.add("TypeScript");
-//        techStack.add("Spring");
+//        List<String> postTechStack = new ArrayList<>();
+//        postTechStack.add("Java");
+//        postTechStack.add("JavaScript");
+//        postTechStack.add("Python");
+//        postTechStack.add("Node");
+//        postTechStack.add("cpp");
+//        postTechStack.add("Flask");
+//        postTechStack.add("Django");
+//        postTechStack.add("React");
+//        postTechStack.add("php");
+//        postTechStack.add("Swift");
+//        postTechStack.add("Kotlin");
+//        postTechStack.add("TypeScript");
+//        postTechStack.add("Spring");
 //        List<String> online = new ArrayList<>();
 //        List<String> term = new ArrayList<>();
 //        List<String> subject = new ArrayList<>();
@@ -297,32 +297,31 @@ public class TestDataRunner implements ApplicationRunner {
 //            String per = person.get(getRandomIndex(person.size()));
 //            String wan = want.get(getRandomIndex(want.size()));
 //            String title = on + ter + sub + per + wan;
-//            String summary = per + wan;
 //            int totalMember = member.get(getRandomIndex(member.size()));
 //            String contents = on + ter + sub + per + totalMember + "명 " + wan;
-//            LocalDate startDate = LocalDate.now().plusDays(getRandomIndex(20));
-//            LocalDate endDate = LocalDate.now().plusDays(getRandomIndex(100));
+//            Timestamp startDate = Timestamp.valueOf(LocalDate.now().plusDays(getRandomIndex(20)).atStartOfDay());
+//            Timestamp endDate = Timestamp.valueOf(startDate.toLocalDateTime().plusDays(1 + getRandomIndex(100)));
 //            Set<String> randomTechStack = new HashSet<>();
 //            int techCount = getRandomIndex(4);
 //            while(randomTechStack.size() < techCount){
-//                randomTechStack.add(techStack.get(getRandomIndex(techStack.size())));
+//                randomTechStack.add(postTechStack.get(getRandomIndex(postTechStack.size())));
 //            }
 //            PostRequestDto postRequestDto = new PostRequestDto(
 //                    title,
-//                    summary,
 //                    contents,
 //                    totalMember,
 //                    "모집중",
-//                    startDate.toString(),
-//                    endDate.toString(),
-//                    new ArrayList<>(randomTechStack)
+//                    startDate,
+//                    endDate,
+//                    new ArrayList<>(randomTechStack),
+//                    "url"
 //            );
 //
 //            postService.writePost(postRequestDto, curUserList.get(getRandomIndex(curUserList.size())).getSnsId());
 //        }
-    }
-
-    private int getRandomIndex(int size) {
-        return (int) ((Math.random() * 100) % size);
-    }
-}
+//    }
+//
+//    private int getRandomIndex(int size) {
+//        return (int) ((Math.random() * 100) % size);
+//    }
+//}
