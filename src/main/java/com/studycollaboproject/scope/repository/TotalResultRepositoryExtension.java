0f7a24de2b8206ff1df27f191d42c1684ebfeda8
woodimora <woodimora@gmail.com>
2021-12-18 20:05:27 +0900

    [수정] 이미지를 S3 서버로 업로드 하도록 변경.

--- src/main/java/com/studycollaboproject/scope/repository/TotalResultRepositoryExtension.java
+++ src/main/java/com/studycollaboproject/scope/repository/TotalResultRepositoryExtension.java
@@ -1,9 +1,5 @@
 package com.studycollaboproject.scope.repository;
 
-import com.studycollaboproject.scope.model.Post;
-import com.studycollaboproject.scope.model.Team;
-import com.studycollaboproject.scope.model.User;
-
 import java.util.List;
 
 public interface TotalResultRepositoryExtension {
